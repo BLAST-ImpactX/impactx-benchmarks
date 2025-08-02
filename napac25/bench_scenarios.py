cac51f22713d16fac1e0d7318f389c5794bef0da
Axel Huebl <axel.huebl@plasma.ninja>
2025-08-01 20:00:33 -0700

    CPU/GPU, Store Results, Best of 5

--- napac25/bench_scenarios.py
+++ napac25/bench_scenarios.py
@@ -5,8 +5,11 @@
 # - jinja2
 # - git
 
+import os
 import re
+import socket
 import subprocess
+import sys
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -50,12 +53,13 @@ code_configs = {
         "env_name": "benchmark-gpu",
         "env_file": "benchmark-gpu-conda.yaml",
     },
-    # TODO: Cheetah cpu/gpu device
     "cheetah": {
         "code": "cheetah",
         "version": "0.7.4",
         "compile_mode": "none",  # https://docs.pytorch.org/docs/stable/generated/torch.compile.html#torch.compile
         "compile_backend": "none",
+        "device": "cpu",
+        "dtype": "torch.float32",
         "env_name": "benchmark-cpu",
         "env_file": "benchmark-cpu-conda.yaml",
     },
@@ -64,9 +68,44 @@ code_configs = {
         "version": "0.7.4",
         "compile_mode": "default",  # TODO: try also "max-autotune" on CPUs https://docs.pytorch.org/docs/stable/generated/torch.compile.html#torch.compile
         "compile_backend": "inductor",  # TODO: try also "ipex" on Intel CPUs
+        "device": "cpu",
+        "dtype": "torch.float32",
         "env_name": "benchmark-cpu",
         "env_file": "benchmark-cpu-conda.yaml",
-    }
+    },
+    "cheetah-gpu": {
+        "code": "cheetah",
+        "version": "0.7.4",
+        "compile_mode": "none",  # https://docs.pytorch.org/docs/stable/generated/torch.compile.html#torch.compile
+        "compile_backend": "none",
+        "device": "gpu",
+        "dtype": "float32",
+        "env_name": "benchmark-gpu",
+        "env_file": "benchmark-gpu-conda.yaml",
+    },
+    "cheetah-gpu-compiled-default": {
+        "code": "cheetah",
+        "version": "0.7.4",
+        "compile_mode": "default",  # TODO: try also "max-autotune" on CPUs https://docs.pytorch.org/docs/stable/generated/torch.compile.html#torch.compile
+        "compile_backend": "inductor",  # TODO: try also "ipex", "onnxrt" on (Intel) CPUs; "inductor", "cudagraphs", "onnxrt" on GPU
+        "device": "gpu",
+        "dtype": "float32",
+        "env_name": "benchmark-gpu",
+        "env_file": "benchmark-gpu-conda.yaml",
+    },
+}
+
+code_configs = {
+    "cheetah": {
+        "code": "cheetah",
+        "version": "0.7.4",
+        "compile_mode": "none",  # https://docs.pytorch.org/docs/stable/generated/torch.compile.html#torch.compile
+        "compile_backend": "none",
+        "device": "cpu",
+        "dtype": "torch.float32",
+        "env_name": "benchmark-cpu",
+        "env_file": "benchmark-cpu-conda.yaml",
+    },
 }
 
 
@@ -120,7 +159,7 @@ def find_timing_lines(stdout, patterns):
     return time_ns
 
 
-def bench(code_config, npart):
+def bench(code_config, npart, nruns=5):
     time_ns = None
 
     config = code_configs[code_config]
@@ -131,33 +170,82 @@ def bench(code_config, npart):
 
     render_script("htu", "run_cheetah_impactx.py.jinja", data)
 
-    command = f"{conda} run -n {env_name} python run_cheetah_impactx.py"
-    result = subprocess.run(command, shell=True, check=False, capture_output=True)
-    stdout = result.stdout.decode("utf-8").split("\n")
-    stderr = result.stderr.decode("utf-8")
+    time_ns_min = {"track_ns": sys.float_info.max}
 
-    print(f"'{code_config}' standard output:", stdout)
-    # print("'{code_config}' standard error:", stderr)
-    # print("'{code_config}' return code:", result.returncode)
+    for nrun in range(nruns):
 
-    if result.returncode != 0:
-        raise RuntimeError(f"config '{code_config}' failed with: {stderr}")
+        command = f"{conda} run -n {env_name} python run_cheetah_impactx.py"
+        result = subprocess.run(command, shell=True, check=False, capture_output=True)
+        stdout = result.stdout.decode("utf-8").split("\n")
+        stderr = result.stderr.decode("utf-8")
 
-    patterns = {
-        "track_ns": re.compile(r"Track: (.*)ns"),
-    }
-    time_ns = find_timing_lines(stdout, patterns)
+        print(f"'{code_config}' w/ {npart} particles standard output:", stdout)
+        # print("'{code_config}' w/ {npart} particles standard error:", stderr)
+        # print("'{code_config}' w/ {npart} particles return code:", result.returncode)
 
-    return time_ns
+        if result.returncode != 0:
+            raise RuntimeError(f"config '{code_config}' w/ {npart} particles failed with: {stderr}")
+
+        patterns = {
+            "track_ns": re.compile(r"Track: (.*)ns"),
+        }
+        time_ns = find_timing_lines(stdout, patterns)
+
+        time_ns_min["track_ns"] = min(time_ns_min["track_ns"], time_ns["track_ns"])
+
+    return time_ns_min
+
+
+
+def merge_dicts(original, new_dict):
+    """Overwrite existing keys in original and insert new ones"""
+    from collections.abc import Mapping
+    from copy import deepcopy
+
+    result = deepcopy(original)
+
+    for key, value in new_dict.items():
+        if isinstance(value, Mapping):
+            result[key] = merge_dicts(result.get(key, {}), value)
+        else:
+            result[key] = deepcopy(new_dict[key])
+
+    return result
+
+
+def save_timings(timings):
+    """Open an existing timings file and update it"""
+    import yaml
+
+    timing_file = "timings.yaml"
+
+    # read existing file
+    if os.path.exists(timing_file):
+        with open(timing_file, "r") as file:
+            previous_data = yaml.safe_load(file)
+    else:
+        previous_data = {}
+
+    # update benchmarks
+    merged_timings = merge_dicts(previous_data, timings)
+
+    # save
+    with open(timing_file, "w") as file:
+        yaml_format = yaml.dump(merged_timings, default_flow_style=False)
+        file.write(yaml_format)
+
+    print(yaml_format)
+
+
+hn = socket.gethostname()
+timings = {}
 
 # HTU Benchmark
 #
-timings = {}
-for npart in nparts:
-    str_npart = str(npart)
-    timings[str_npart] = {}
-
 for code_config, _ in code_configs.items():  # TODO: CPU 1-N threads, GPU
+    timings[code_config] = {}
+    timings[code_config][hn] = {}
+    timings[code_config][hn]["config"] = code_configs[code_config]
 
     # We vary the number of CPU threads, to see if the code benefits from threading (expectation: linear speedup).
     # We compare CPU and GPU runs, to see if the code benefits from GPU acceleration (expectation: ~10x, depending on hardware).
@@ -167,13 +255,10 @@ for code_config, _ in code_configs.items():  # TODO: CPU 1-N threads, GPU
     # to see if a code can make efficient use of L1/L2/L3 caches
     for npart in nparts:
         str_npart = str(npart)
-        timings[str_npart][code_config] = bench(code_config, npart)
+        timings[code_config][hn][str_npart] = bench(code_config, npart)
+        timings[code_config][hn][str_npart]
 
         # calculate particle push time
-        timings[str_npart][code_config]["push_per_sec"] = npart / timings[str_npart][code_config]["track_ns"] * 1e9
+        timings[code_config][hn][str_npart]["push_per_sec"] = npart / timings[code_config][hn][str_npart]["track_ns"] * 1e9
 
-# TODO: proper storage and plotting
-import json
-print(json.dumps(timings, indent=4))
-
-print(timings["100000"]["cheetah"]["push_per_sec"] / timings["100000"]["cheetah-compiled-default"]["push_per_sec"])
+save_timings(timings)
